7ebeea5aa6dbb71cdc9b456db2d2b8d12bd259af
Tudypie <33548577+Tudypie@users.noreply.github.com>
2024-01-28 15:09:12 +0200

    Finsishing

--- GGJ_MakeMeLaugh_UnityProject/Assets/Scripts/GameManagers/SceneManagement.cs
+++ GGJ_MakeMeLaugh_UnityProject/Assets/Scripts/GameManagers/SceneManagement.cs
@@ -12,6 +12,23 @@ public class SceneManagement : MonoBehaviour
         Instance = this;
     }
 
+    public void Start()
+    {
+        SceneManager.LoadScene("WhiteRoom", LoadSceneMode.Additive);
+        SceneManager.LoadScene("FirstMonitorRoom", LoadSceneMode.Additive);
+    }
+
+    public void RestartScene()
+    {
+        StartCoroutine(Restart());
+    }
+
+    IEnumerator Restart()
+    {
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene("Game");
+    }
+
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
